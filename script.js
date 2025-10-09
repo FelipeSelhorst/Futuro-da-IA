fb28bde249e1d0024f0985db332aa5f0bacb004a
Felipe <selhorstdelimafelipe@gmail.com>
2025-10-09 11:45:56 -0300

    x

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-var menuButton = document.getElementById("menu-button");
-var menu = document.getElementById("menu-options");
+const mainBox = document.querySelector(".main-box");
+const qBox = document.querySelector(".Q-box");
+const aBox = document.querySelector(".A-box");
+const rBox = document.querySelector(".R-box");
+const rText = document.querySelector(".R-text");
 
-menuButton.addEventListener("click", function() {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
+const lista = [item1, item2]
+
+const Perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 3",
+    alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+    ]
+  },
+]
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+  if (atual >= perguntas[atual]) {
+    mostraResultado();
+    return;
   }
-});
+  perguntaAtual = Perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativas;
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal = afirmacoes;
+  atual++;
+  mostraPergunta();
+}
+
+function mostraResposta() {
+  caixaPerguntas.textContent = "conteudo";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
